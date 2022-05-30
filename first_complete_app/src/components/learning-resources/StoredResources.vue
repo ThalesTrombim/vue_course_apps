13b8e309f5e9589f3dc952a3c92b28057b89feb6
ThalesTrombim <thalestr@outlook.com>
2022-05-30 20:58:23 -0300

    resources app

---
+++ first_complete_app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <LearningResource 
+      v-for="res in resource" :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></LearningResource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+
+  export default {
+    props: ['resource'],
+    components: {
+      LearningResource
+    }
+  }
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
---
+++ first_complete_app/src/components/learning-resources/TheResource.vue
@@ -0,0 +1,14 @@
+<template>
+  <base-card>
+    <base-button>Stored Resources</base-button>
+    <base-button>Add Resource</base-button>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    
+  }
+}
+</script>
