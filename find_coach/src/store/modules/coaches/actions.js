e0b7b095574e98e61ca3df15eb8b030aceba9f67
ThalesTrombim <thalestr@outlook.com>
2022-06-25 13:52:13 -0300

    feat: send data to firebase

--- find_coach/src/store/modules/coaches/actions.js
+++ find_coach/src/store/modules/coaches/actions.js
@@ -1,7 +1,8 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
+
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.description,
@@ -9,6 +10,24 @@ export default {
       areas: data.areas,
     };
 
-    context.commit('registerCoach', coachData);
+    const response = await fetch(
+      `https://main-vue-thales-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData),
+      }
+    );
+    console.log(response);
+
+    // const responseData = await response.json();
+
+    // if (!response.ok) {
+    //   // error
+    // }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId,
+    });
   },
 };
