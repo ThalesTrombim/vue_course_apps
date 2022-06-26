e9c956453616d9fd976d229e0f63d68e7e648eb0
ThalesTrombim <thalestr@outlook.com>
2022-06-26 20:19:48 -0300

    feat: get data from firebasa

--- find_coach/src/store/modules/coaches/actions.js
+++ find_coach/src/store/modules/coaches/actions.js
@@ -30,4 +30,33 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://main-vue-thales-default-rtdb.firebaseio.com/coaches.json`
+    );
+
+    const responseData = await response.json();
+
+    // if (!response.ok) {
+    //   // error
+    // }
+
+    console.log(responseData);
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    console.log(coaches);
+    context.commit('setCoaches', coaches);
+  },
 };
